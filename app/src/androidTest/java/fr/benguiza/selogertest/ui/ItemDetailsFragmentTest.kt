2548f84f67492f102fec4224e6162cfafd9a17c7
taoufik <taoufik.benguiza@external.engie.com>
2021-03-22 13:58:38 +0100

    refacto name tests

--- app/src/androidTest/java/fr/benguiza/selogertest/ui/ItemDetailsFragmentTest.kt
+++ app/src/androidTest/java/fr/benguiza/selogertest/ui/ItemDetailsFragmentTest.kt
@@ -1,38 +1,32 @@
 package fr.benguiza.selogertest.ui
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.recyclerview.widget.RecyclerView
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.contrib.RecyclerViewActions
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import fr.benguiza.domainlayer.item.GetItemsUseCase
-import fr.benguiza.domainlayer.item.RetrieveItemsUseCase
-import fr.benguiza.domainlayer.item.SaveToDataBaseItemsUseCase
+import com.schibsted.spain.barista.assertion.BaristaVisibilityAssertions.assertDisplayed
+import fr.benguiza.domainlayer.item.GetItemByIdUseCase
+import fr.benguiza.domainlayer.item.GetLocalItemByIdUseCase
 import fr.benguiza.selogertest.R
 import fr.benguiza.selogertest.test.BaseTest
 import fr.benguiza.selogertest.test.ItemUiData
 import fr.benguiza.selogertest.test.TestFragmentRule
 import fr.benguiza.selogertest.tools.extensions.AppBuildConfig
-import fr.benguiza.selogertest.ui.item.listing.ItemsListingFragment
-import fr.benguiza.selogertest.ui.item.listing.ItemsListingViewModel
+import fr.benguiza.selogertest.tools.extensions.bundle
+import fr.benguiza.selogertest.ui.item.details.ARG_ITEM_ID
+import fr.benguiza.selogertest.ui.item.details.ItemDetailsFragment
+import fr.benguiza.selogertest.ui.item.details.ItemsDetailsViewModel
 import io.mockk.every
 import io.mockk.mockk
 import io.mockk.mockkObject
 import io.reactivex.Observable
-import it.xabaras.android.espresso.recyclerviewchildactions.RecyclerViewChildActions.Companion.childOfViewAtPositionWithMatcher
-import org.hamcrest.CoreMatchers.allOf
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class ItemDetailsFragmentTest : BaseTest() {
 
-    private var retrieveItemsUseCase: RetrieveItemsUseCase = mockk()
-    private var saveToDataBase: SaveToDataBaseItemsUseCase = mockk()
-    private var getItemsUseCase: GetItemsUseCase = mockk()
+    private var getItemByIdUseCase: GetItemByIdUseCase = mockk()
+    private var getLocalItemByIdUseCase: GetLocalItemByIdUseCase = mockk()
     private lateinit var data: ItemUiData
+    private lateinit var viewModele: ItemsDetailsViewModel
 
     @Rule
     @JvmField
@@ -44,21 +38,28 @@ class ItemDetailsFragmentTest : BaseTest() {
 
     @Before
     fun setUp() {
-        data = ItemUiData()
 
-        every { retrieveItemsUseCase.execute() } returns Observable.just(data.domain.items)
-        every { getItemsUseCase.execute() } returns Observable.just(data.domain.items)
+        data = ItemUiData()
+        viewModele = ItemsDetailsViewModel(getItemByIdUseCase, getLocalItemByIdUseCase)
+        every { getItemByIdUseCase.execute(1) } returns Observable.just(data.domain.items.first())
+        every { getLocalItemByIdUseCase.execute(1) } returns Observable.just(data.domain.items.first())
         mockkObject(AppBuildConfig)
         every { AppBuildConfig.getTestingValue() } returns true
-
     }
 
     private fun showFragment() {
-        with(ItemsListingFragment()) {
-            viewModel = ItemsListingViewModel(retrieveItemsUseCase, saveToDataBase, getItemsUseCase)
+        with(ItemDetailsFragment()) {
+            viewModel = viewModele
+            arguments = bundle {
+                putInt(
+                    ARG_ITEM_ID,
+                    1
+                )
+            }
             fragmentRule.setFragment(this)
         }
     }
+
 
     @Test
     fun showItemDetails() {
@@ -66,24 +67,11 @@ class ItemDetailsFragmentTest : BaseTest() {
         showFragment()
 
         // Then
-        onView(withId(R.id.view_items_list))
-            .perform(RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(0))
-            .check(
-                matches(
-                    allOf(
-                        childOfViewAtPositionWithMatcher(
-                            R.id.view_item_city,
-                            0,
-                            withText("A")
-                        ),
-                        childOfViewAtPositionWithMatcher(
-                            R.id.view_item_price,
-                            0,
-                            withText("123,00")
-                        )
-                    )
-                )
-            )
+        assertDisplayed(R.id.view_item_property_rooms, "3 piéces")
+        assertDisplayed(R.id.view_item_property_bedrooms, "3 chambres")
+        assertDisplayed(R.id.city_text, "A")
+        assertDisplayed(R.id.view_item_professional, "professional")
+        assertDisplayed(R.id.view_item_property_type, "Type de propriété : propertyType")
 
     }
 
